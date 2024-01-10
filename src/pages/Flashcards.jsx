2153e640012ebde74b87fc20082b63be5fa2a421
kizido <kieranking3555@gmail.com>
2024-01-09 18:21:05 -0800

    feat: setup basic layout and styling of flashcards page

--- src/pages/Flashcards.jsx
+++ src/pages/Flashcards.jsx
@@ -1,24 +1,45 @@
-import React, { useState } from "react";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import wordList from "../vocab_lists/spanishnouns.json";
+import * as flashcardStyles from "../style/Flashcards.module.css";
 
 const getRandomInt = (max) => {
-  const num = Math.floor(Math.random() * max);
-  return num;
+  return Math.floor(Math.random() * max);
 };
+const getRandomWordIndex = () => {
+  return getRandomInt(wordList.length);
+}
 
 export default function Flashcards() {
   let { vocabAmount } = useParams();
   vocabAmount = parseInt(vocabAmount);
 
-  const [word, setWord] = useState();
+  const [vocabLeft, setVocabLeft] = useState(vocabAmount);
+  const [wordIndex, setWordIndex] = useState();
+  const [wordsToLearn, setWordsToLearn] = useState([]);
+
+  useEffect(() => {
+    setWordIndex(getRandomWordIndex());
+  }, [])
 
   return (
-    <div>
-      <h1>Current Word: {word ?? 'N/A'}</h1>
+    <div className={flashcardStyles.pageContainer}>
+      <h1>Vocab Left: {vocabLeft}</h1>
+      <div className={flashcardStyles.flashcardContainer}>
+        <h2>{wordList[getRandomWordIndex()].spanish}</h2>
+      </div>
+      <div className={flashcardStyles.wordConfirmationContainer}>
+        <button className={flashcardStyles.wordPassButton}>
+          I know this already...
+        </button>
+        <button className={flashcardStyles.wordConfirmButton}>
+          Let's learn it!
+        </button>
+      </div>
+      {/* <h1>Current Word: {word ?? 'N/A'}</h1>
       <button onClick={() => setWord(wordList[getRandomInt(wordList.length)].spanish)}>
         Select New Word
-      </button>
+      </button> */}
     </div>
   );
 }

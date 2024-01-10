7df99e4e6cadbe7496e5cc333392f68610fc8a38
kizido <kieranking3555@gmail.com>
2024-01-09 16:52:37 -0800

    feat: generate random spanish word from wordlist json

--- src/pages/Flashcards.jsx
+++ src/pages/Flashcards.jsx
@@ -1,7 +1,24 @@
-import React from 'react'
+import React, { useState } from "react";
+import { useParams } from "react-router-dom";
+import wordList from "../vocab_lists/spanishnouns.json";
+
+const getRandomInt = (max) => {
+  const num = Math.floor(Math.random() * max);
+  return num;
+};
 
 export default function Flashcards() {
+  let { vocabAmount } = useParams();
+  vocabAmount = parseInt(vocabAmount);
+
+  const [word, setWord] = useState();
+
   return (
-    <div>Flashcards</div>
-  )
+    <div>
+      <h1>Current Word: {word ?? 'N/A'}</h1>
+      <button onClick={() => setWord(wordList[getRandomInt(wordList.length)].spanish)}>
+        Select New Word
+      </button>
+    </div>
+  );
 }

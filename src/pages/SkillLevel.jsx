9408a20beb5ea22278fd11d82b22c3fa0feb1858
kizido <kieranking3555@gmail.com>
2024-01-16 21:11:16 -0800

    feat: basic routing from skill level to vocab amount

--- src/pages/SkillLevel.jsx
+++ src/pages/SkillLevel.jsx
@@ -1,25 +1,38 @@
 import { useNavigate } from "react-router-dom";
 import * as styles from "../style/SkillLevel.module.css";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function SkillLevel() {
-    let navigate = useNavigate();
+  let navigate = useNavigate();
 
-    const [skillLevel, setSkilllevel] = useState('');
+  const [skillLevel, setSkilllevel] = useState("");
+
+  const goToVocabAmount = () => {
+    navigate(`/${skillLevel}/vocabAmount`);
+  };
+
+  useEffect(() => {
+    if(skillLevel != "") {
+        goToVocabAmount();
+    }
+  }, [skillLevel])
 
   return (
     <div className={styles.skillLevelSection}>
-      <div className={styles.skillLevelBox} onClick={() => navigate(`/${skillLevel}/vocabAmount`)}>
+      <div
+        className={styles.skillLevelBox}
+        onClick={() => setSkilllevel('beginner')}
+      >
         <div className={styles.skillLevelTitle}>
           <h2>Beginner</h2>
         </div>
       </div>
-      <div className={styles.skillLevelBox}>
+      <div className={styles.skillLevelBox} onClick={() => setSkilllevel('intermediate')}>
         <div className={styles.skillLevelTitle}>
           <h2>Intermediate</h2>
         </div>
       </div>
-      <div className={styles.skillLevelBox}>
+      <div className={styles.skillLevelBox} onClick={() => setSkilllevel('advanced')}>
         <div className={styles.skillLevelTitle}>
           <h2>Advanced</h2>
         </div>
